d50037361ee7c31b66a645d70aad0e4e31412e60
arigehan <94726278+arigehan@users.noreply.github.com>
2022-06-22 21:37:51 -0400

    added trend

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   const [bloodGlucose, setBloodGlucose] = useState(0);
+  const [sugarTrend, setSugarTrend] = useState(0);
 
   const updateSGV = () => {
     axios.get('http://cgmari.herokuapp.com/api/v1/entries/current', {
@@ -15,6 +16,7 @@ export default function App() {
         let data = response.data
         // console.log(data[0].sgv);
         setBloodGlucose(data[0].sgv);
+        setSugarTrend(data[0].trend)
         // console.log(data);
       })
       .catch(error => {
@@ -29,20 +31,39 @@ export default function App() {
       })
   }
 
+  var trendLine = 'Flat';
+  if (sugarTrend === 1) {
+    trendLine = 'Going Up Fast (i think, 1)';
+  } else if (sugarTrend === 2) {
+    trendLine = 'Going Up (i think, 2)'
+  } else if (sugarTrend === 3) {
+    trendLine = 'Slightly Going Up'
+  } else if (sugarTrend === 4) {
+    trendLine = 'Flat'
+  } else if (sugarTrend === 5) {
+    trendLine = 'Slightly Going Down'
+  } else if (sugarTrend === 6) {
+    trendLine = 'Going Down (i think, 6)'
+  } else if (sugarTrend === 7) {
+    trendLine = 'Going Down Fast (i think, 7)'
+  }else {
+    trendLine = 'Unknown';
+  };
+
   useEffect(() => {
     updateSGV();
     const timer = setInterval(() => {
       updateSGV();
-    }, 320000)
+    }, 220000)
 
     return () => clearInterval(timer);
   }, [])
-  
 
   return (
     <View style={styles.container}>
-      <Text>Current Blood Glucose:</Text>
-      <Text>{bloodGlucose}</Text>
+      <Text style={{ fontSize: 30, color: '#1f95bd' }}>Current Blood Glucose:</Text>
+      <Text style={{ fontSize: 50, color:'#08518e' }}>{bloodGlucose}</Text>
+      <Text style={{ fontSize: 30, color: '#1f95bd' }}>{trendLine}</Text>
       <StatusBar style="auto" />
     </View>
   );
